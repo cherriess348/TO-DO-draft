d98939ccfe77194d984ab92a9d0d4f7e98f7248a
chary_wind <vandasochary@gmail.com>
2025-12-12 02:04:55 +0700

    new develop

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Doing from './components/Doing.jsx'
+import Account from "./components/Account.jsx";
+import Sidebar from "./components/Sidebar.jsx";
+import Todo from "./components/Todo.jsx";
+
+ 
  
 
 function App() {
@@ -10,15 +14,16 @@ function App() {
 
   return (
     <>
-    <body  className='bg-orange-300'>
-       <div className='flex   '>
-          
-         
-        <Doing ></Doing>
-        
-        
-         
-    </div>
+    <body  className='bg-blue-950 text-amber-50 font-serif
+'>
+      <div className='m-0 flex flex-col items-center mt-9'>
+             <Todo></Todo>
+            <br /><br /><br /><br />
+            <Sidebar></Sidebar>
+            <Account></Account>
+      </div>
+      
+      
     </body>
    
         
